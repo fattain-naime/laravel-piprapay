6a31b6a9a0c260dd17bcd03f0bc69dc48a2f36f1
Fattain Naime <iamnaime@builderhall.com>
2025-09-27 07:40:18 +0600

    laravel-piprapay

---
+++ src/Exceptions/PipraPayApiException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FattainNaime\PipraPay\Exceptions;
+
+use Exception;
+
+class PipraPayApiException extends Exception
+{
+    // You can add custom logic here if needed in the future.
+}
